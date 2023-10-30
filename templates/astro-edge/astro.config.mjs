024fb1476084bfa0b00cb9bf7e3683ba7f151627
Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2023-10-30 09:28:01 -0700

    feat: add Edge deployment (#1)
    * feat: add edge template
    
    * fix: remove bind from build to prevent dup builds
    
    * fix: remove bind from hybrid build
    
    * feat: check headers before overriding charset
    
    * chore: reverts sst versions to published

---
+++ templates/astro-edge/astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config'
+import prefetch from '@astrojs/prefetch'
+import robotsTxt from 'astro-robots-txt'
+import sitemap from '@astrojs/sitemap'
+import aws from 'astro-sst/edge'
+import mdx from '@astrojs/mdx'
+
+const site = import.meta.env.DEV
+  ? 'http://localhost:3000'
+  : import.meta.env.VITE_SITE_URL
+
+const crawlerPolicy = [{ userAgent: '*', disallow: ['/'] }]
+
+export default defineConfig({
+  site,
+  integrations: [
+    mdx(),
+    sitemap(),
+    prefetch(),
+    robotsTxt({ policy: crawlerPolicy })
+  ],
+  output: 'server',
+  redirects: {
+    '/old-blog/': '/blog',
+    '/old-blog/[...slug]': '/blog/[...slug]'
+  },
+  adapter: aws({
+    // responseMode: 'buffer',
+    // serverRoutes: ['/register', '/methods.json']
+  })
+})
---
+++ templates/astro-edge/src/consts.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Astro + SST: SSR'
+export const SITE_DESCRIPTION =
+  'Astro with server-side rendering deployed using SST.'
